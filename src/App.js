6422590ad2a256856f6ece7a8ebe8a953ed5c42a
Harsh <harshj7.net@gmail.com>
2023-06-19 14:06:28 +0530

    add basename react-router-first in index.js and remove unused import browserrouter in app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import {
-    BrowserRouter,
     Routes,
     Route
 } from "react-router-dom";
